a18fbd9c8b55c2b7888fbf28153a3999853cb2e3
Oleksii Ivanenko <ivalextar@gmail.com>
2024-08-08 17:45:19 +0200

    add deserialization

--- hw8_bot_pickle.py
+++ hw8_bot_pickle.py
@@ -225,7 +225,12 @@ def save_data(book, filename="addressbook.pkl"):
     with open(filename, "wb") as f:
         pickle.dump(book, f)
 
-
+def load_data(filename="addressbook.pkl"):
+    try:
+        with open(filename, "rb") as f:
+            return pickle.load(f)
+    except FileNotFoundError:
+        return AddressBook()  # Return a new address book if the file is not found 
 
 # Main function
 def main():
